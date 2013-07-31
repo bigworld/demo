7873eb61d1c2622e1e6b359d3df8ff0f3dcd8ae0
bigworld <hom_zhang1205@163.com>
2013-07-31 12:18:04 +0800

    add string.php

---
+++ string.php
@@ -0,0 +1,15 @@
+<?php
+function random_string($length = 15, $incase = true){    
+	$pool = 'abcdefghijkmnpqrstuvwxyz';    
+	$pool .= '23456789';    
+	if ($incase) {        
+		$pool .= 'ABCDEFGHIJKMNPQRSTUVWXYZ';    
+	}    
+	$poollen = strlen($pool);    
+	mt_srand((double) microtime() * 1000000);    
+	$string = '';    
+	for ($i = 0; $i < $length; $i++) {        
+		$string .= substr($pool, (mt_rand() % ($poollen)), 1);    
+	}    
+	return $string;
+} 
